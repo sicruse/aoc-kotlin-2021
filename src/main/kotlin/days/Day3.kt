f2c80a6734e4a4a1845941fce87fb573e64069e2
Si Cruse <si@sicruse.com>
2021-12-04 18:21:08 -0500

    refactored Day3

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -1,17 +1,25 @@
 package days
 
-import kotlin.math.sign
-
 class Day3 : Day(3) {
 
     class Diagnostics(data: List<String>) {
         private val signals: List<Int> by lazy { data.map { Integer.parseInt(it, 2); } }
-        private val bitwidth = data.first().length
+        private val numberOfBits = data.first().length
 
         // sum the 1 bits across each column of data and if greater than half of the total number of rows then the answer is 1 otherwise 0
         // assume that there is never an even split of bits in the rows
 
         enum class Diagnostic {
+            gamma {
+                override fun process(signalset: List<Int>, bitindex: Int): Int {
+                    val threshold = signalset.count() / 2
+                    return (bitindex downTo 1).fold(0) { acc, index ->
+                        val countofones = signalset.sumOf { it shr index - 1 and 1 }
+                        val bit = if (countofones > threshold) 1 else 0
+                        acc or (bit shl (index - 1))
+                    }
+                }
+            },
             oxygen {
                 override fun process(signalset: List<Int>, bitindex: Int): Int {
                     val signals = mostCommon(signalset, bitindex)
@@ -48,26 +56,20 @@ class Day3 : Day(3) {
             }
         }
 
-        val gamma by lazy {
-            val threshold = data.count() / 2
-
-            (bitwidth downTo 1).fold(0) { acc, bitindex ->
-                val countofones = signals.sumOf { it shr bitindex - 1 and 1 }
-                val bit = if (countofones > threshold) 1 else 0
-                acc or (bit shl (bitindex - 1))
-            }
+        private fun epsilonFromGamma(gamma: Int): Int {
+            val bitmask = -1 ushr Int.SIZE_BITS - numberOfBits
+            return gamma.inv() and bitmask
         }
 
-        val epsilon by lazy {
-            val bitmask = -1 ushr Int.SIZE_BITS - bitwidth
-            gamma.inv() and bitmask
+        val powerconsumption by lazy {
+            val gamma = Diagnostic.gamma.process(signals, numberOfBits)
+            val epsilon = epsilonFromGamma(gamma)
+            gamma * epsilon
         }
-
-        val powerconsumption by lazy { gamma * epsilon }
 
         val lifesupport by lazy {
-            val oxygen = Diagnostic.oxygen.process(signals, bitwidth)
-            val co2 = Diagnostic.co2.process(signals, bitwidth)
+            val oxygen = Diagnostic.oxygen.process(signals, numberOfBits)
+            val co2 = Diagnostic.co2.process(signals, numberOfBits)
             oxygen * co2
         }
 
